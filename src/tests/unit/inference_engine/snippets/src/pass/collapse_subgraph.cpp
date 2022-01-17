ea97a342b8f67d15fcf4ac28de2f58defacb13f9
Ivan Novoselov <ivan.novoselov@intel.com>
2022-01-17 11:23:49 +0300

    Added two tokenization subgraphs

--- src/tests/unit/inference_engine/snippets/src/pass/collapse_subgraph.cpp
+++ src/tests/unit/inference_engine/snippets/src/pass/collapse_subgraph.cpp
@@ -13,10 +13,10 @@
 #include <snippets/pass/collapse_subgraph.hpp>
 #include <snippets/op/subgraph.hpp>
 
-#include <transformations/init_node_info.hpp>
+//#include <transformations/init_node_info.hpp>
 
 #include "common_test_utils/ngraph_test_utils.hpp"
-#include "graph_comparator.hpp"
+//#include "graph_comparator.hpp"
 //#include "include/pass/collapse_subgraph.hpp"
 
 using namespace testing;
@@ -100,9 +100,6 @@ class SnippetsCollapseSubgraphTests : public TransformationTestsF {
 public:
     void run(bool serialize = false) {
         ASSERT_TRUE(function);
-        manager.register_pass<EnumerateNodes>();
-        manager.register_pass<TokenizeSnippets>();
-        manager.register_pass<SnippetsRestoreResultInputName>();
         if (serialize) {
             auto formatName = [](const std::string& original_name) {
                 std::string name(original_name);
@@ -117,88 +114,29 @@ public:
                 name = "subgraph";
             manager.register_pass<ov::pass::Serialize>(name + ".xml", name + ".bin");
         }
+        manager.register_pass<EnumerateNodes>();
+        manager.register_pass<TokenizeSnippets>();
+        manager.register_pass<SnippetsRestoreResultInputName>();
     }
 };
 
-TEST_F(SnippetsCollapseSubgraphTests, StartSubgraphMultipleOutputs) {
-    function = ngraph::builder::subgraph::AddSubtractMultiplyFunction::getOriginal();
-    function_ref = ngraph::builder::subgraph::AddSubtractMultiplyFunction::getReference();
+TEST_F(SnippetsCollapseSubgraphTests, EltwiseSubgraph) {
+    function = ngraph::builder::subgraph::EltwiseFunction::getOriginal();
+    function_ref = ngraph::builder::subgraph::EltwiseFunction::getReference();
     run(true);
 }
 
-TEST(TransformationTests, TokenizeMulAddSubgraph) {
-    std::shared_ptr<Model> f(nullptr), f_ref(nullptr);
-    {
-        auto data_1 = std::make_shared<op::v0::Parameter>(element::f32, Shape{ 1, 3, 4, 4 });
-        auto data_2 = std::make_shared<op::v0::Parameter>(element::f32, Shape{ 1, 3, 4, 4 });
-        auto non_snippet_op = std::make_shared<op::v0::MatMul>(data_1, data_2);
-
-        auto mul_const_1 = op::v0::Constant::create(element::f32, { 1, 3, 1, 1 }, { 4.f });
-        auto mul_1 = std::make_shared<op::v1::Multiply>(non_snippet_op, mul_const_1);
-        auto add_const_1 = op::v0::Constant::create(element::f32, { 1, 3, 1, 1 }, { 16.4f });
-        auto add_1 = std::make_shared<op::v1::Add>(mul_1, add_const_1);
-        auto elu = std::make_shared<op::v0::Elu>(add_1, 0.01);
-
-        auto mul_const_2 = op::v0::Constant::create(element::f32, { 1, 3, 1, 1 }, { 4.f });
-        auto mul_2 = std::make_shared<op::v1::Multiply>(non_snippet_op, mul_const_2);
-        auto add_const_2 = op::v0::Constant::create(element::f32, { 1, 3, 1, 1 }, { 16.4f });
-        auto add_2 = std::make_shared<op::v1::Add>(mul_2, add_const_2);
-        auto relu = std::make_shared<op::v0::Relu>(add_2);
-
-        auto add = std::make_shared<op::v1::Add>(elu, relu);
-        auto result = std::make_shared<op::v0::Result>(add);
-
-        f = std::make_shared<Model>(ResultVector{ result }, ParameterVector{ data_1, data_2 });
-
-        pass::Manager m;
-        m.register_pass<InitNodeInfo>();
-        m.register_pass<EnumerateNodes>();
-        m.register_pass<TokenizeSnippets>();
-        m.run_passes(f);
-        ASSERT_NO_THROW(check_rt_info(f));
-    }
-    {
-        auto data_1 = std::make_shared<op::v0::Parameter>(element::f32, Shape{ 1, 3, 4, 4 });
-        auto data_2 = std::make_shared<op::v0::Parameter>(element::f32, Shape{ 1, 3, 4, 4 });
-
-        // snippet inputs
-        auto non_snippet_op = std::make_shared<op::v0::MatMul>(data_1, data_2);
-        auto mul_const_1 = op::v0::Constant::create(element::f32, { 1, 3, 1, 1 }, { 4.f });
-        auto add_const_1 = op::v0::Constant::create(element::f32, { 1, 3, 1, 1 }, { 16.4f });
-        auto mul_const_2 = op::v0::Constant::create(element::f32, { 1, 3, 1, 1 }, { 4.f });
-        auto add_const_2 = op::v0::Constant::create(element::f32, { 1, 3, 1, 1 }, { 16.4f });
-
-        // snippet function
-        auto snippet_input = std::make_shared<op::v0::Parameter>(element::f32, Shape{ 1, 3, 4, 4 });
-
-        auto sn_mul_const_1 = std::make_shared<op::v0::Parameter>(element::f32, Shape{ 1, 3, 1, 1 });
-        auto mul_1 = std::make_shared<op::v1::Multiply>(snippet_input, sn_mul_const_1);
-        auto sn_add_const_1 = std::make_shared<op::v0::Parameter>(element::f32, Shape{ 1, 3, 1, 1 });
-        auto add_1 = std::make_shared<op::v1::Add>(mul_1, sn_add_const_1);
-        auto elu = std::make_shared<op::v0::Elu>(add_1, 0.01);
-
-        auto sn_mul_const_2 = std::make_shared<op::v0::Parameter>(element::f32, Shape{ 1, 3, 1, 1 });
-        auto mul_2 = std::make_shared<op::v1::Multiply>(snippet_input, sn_mul_const_2);
-        auto sn_add_const_2 = std::make_shared<op::v0::Parameter>(element::f32, Shape{ 1, 3, 1, 1 });
-        auto add_2 = std::make_shared<op::v1::Add>(mul_2, sn_add_const_2);
-        auto relu = std::make_shared<op::v0::Relu>(add_2);
-
-        auto add = std::make_shared<op::v1::Add>(elu, relu);
-        ParameterVector subgraph_params{ snippet_input, sn_mul_const_1, sn_add_const_1, sn_mul_const_2, sn_add_const_2 };
-        auto snippet_function = std::make_shared<Model>(NodeVector{ add }, subgraph_params);
-
-        ngraph::NodeVector snippet_inputs{ non_snippet_op, mul_const_1, add_const_1, mul_const_2, add_const_2 };
-        auto snippet = std::make_shared<Subgraph>(snippet_inputs, snippet_function);
-        auto result = std::make_shared<op::v0::Result>(snippet);
-
-        f_ref = std::make_shared<Model>(NodeVector{ result }, ParameterVector{ data_1, data_2 });
-    }
-
-    auto res = compare_functions(f, f_ref);
-    ASSERT_TRUE(res.first) << res.second;
+TEST_F(SnippetsCollapseSubgraphTests, MatMulWithEltwiseBranchesSubgraph) {
+    function = ngraph::builder::subgraph::MatMulEltwiseBranchesFunction::getOriginal();
+    function_ref = ngraph::builder::subgraph::MatMulEltwiseBranchesFunction::getReference();
+    run(true);
 }
 
-
+TEST_F(SnippetsCollapseSubgraphTests, AvoidLoopEltwiseSubgraphs) {
+    function = ngraph::builder::subgraph::EltwiseLogLoop::getOriginal();
+    function_ref = ngraph::builder::subgraph::EltwiseLogLoop::getReference();
+    run(true);
+}
 TEST(TransformationTests, DontStartSubgraphSingleOutput) {
     std::shared_ptr<Model> f(nullptr), f_ref(nullptr);
     {
@@ -261,52 +199,6 @@ TEST(TransformationTests, AttachToSubgraph) {
             std::make_shared<Model>(NodeVector{std::make_shared<op::v0::Negative>(inner), inner}, ParameterVector{indata0, indata1}));
         auto concat = std::make_shared<op::v0::Concat>(OutputVector{add->output(0), add->output(1)}, 0);
         f_ref = std::make_shared<Model>(NodeVector{concat}, ParameterVector{data0, data1});
-    }
-
-    auto res = compare_functions(f, f_ref);
-    ASSERT_TRUE(res.first) << res.second;
-}
-
-TEST(TransformationTests, DontAttachToSubgraphIfLoop) {
-    std::shared_ptr<Model> f(nullptr), f_ref(nullptr);
-    {
-        auto data0 = std::make_shared<op::v0::Parameter>(element::f32, Shape{2, 3});
-        auto data1 = std::make_shared<op::v0::Parameter>(element::f32, Shape{1, 3});
-        auto indata0 = std::make_shared<op::v0::Parameter>(element::f32, Shape{2, 3});
-        auto indata1 = std::make_shared<op::v0::Parameter>(element::f32, Shape{1, 3});
-        auto add = std::make_shared<Subgraph>(NodeVector{data0, data1},
-            std::make_shared<Model>(NodeVector{std::make_shared<op::v1::Add>(indata0, indata1)}, ParameterVector{indata0, indata1}));
-        auto log = std::make_shared<op::v0::Log>(add);
-        auto mul = std::make_shared<op::v1::Multiply>(add, log);
-        f = std::make_shared<Model>(NodeVector{mul}, ParameterVector{data0, data1});
-
-        pass::Manager m;
-        m.register_pass<InitNodeInfo>();
-        m.register_pass<EnumerateNodes>();
-        m.register_pass<TokenizeSnippets>();
-        m.run_passes(f);
-        ASSERT_NO_THROW(check_rt_info(f));
-    }
-
-    {
-        auto data0 = std::make_shared<op::v0::Parameter>(element::f32, Shape{2, 3});
-        auto data1 = std::make_shared<op::v0::Parameter>(element::f32, Shape{1, 3});
-        auto indata0 = std::make_shared<op::v0::Parameter>(element::f32, Shape{2, 3});
-        auto indata1 = std::make_shared<op::v0::Parameter>(element::f32, Shape{1, 3});
-        auto add = std::make_shared<Subgraph>(NodeVector{data0, data1},
-            std::make_shared<Model>(NodeVector{std::make_shared<op::v1::Add>(indata0, indata1)}, ParameterVector{indata0, indata1}));
-        auto log = std::make_shared<op::v0::Log>(add);
-        /*
-         * Note that log is not currently supported by snippets, so it won't be converted to subgraph.
-         * Mul will be converted for the "reset" continuation strategy, (present case)
-         * or left as-is for the "abort" continuation strategy
-        */
-        auto add_param = std::make_shared<op::v0::Parameter>(element::f32, add->get_output_shape(0));
-        auto log_param = std::make_shared<op::v0::Parameter>(element::f32, log->get_output_shape(0));
-        auto mul = std::make_shared<Subgraph>(NodeVector{add, log},
-                   std::make_shared<Model>(NodeVector{std::make_shared<op::v1::Multiply>(add_param, log_param)},
-                                                ParameterVector{add_param, log_param}));
-        f_ref = std::make_shared<Model>(NodeVector{mul}, ParameterVector{data0, data1});
     }
 
     auto res = compare_functions(f, f_ref);
