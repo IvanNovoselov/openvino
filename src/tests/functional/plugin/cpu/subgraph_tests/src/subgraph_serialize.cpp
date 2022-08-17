638efa387cef14f77dd31d098d3c82138de13b33
Ivan Novoselov <ivan.novoselov@intel.com>
2022-08-17 15:05:35 +0200

    Add a test for debugging

--- src/tests/functional/plugin/cpu/subgraph_tests/src/subgraph_serialize.cpp
+++ src/tests/functional/plugin/cpu/subgraph_tests/src/subgraph_serialize.cpp
@@ -43,6 +43,68 @@ TEST_F(SubgraphSnippetSerializationTest, SerializeSubgraph) {
     ASSERT_TRUE(results.valid) << results.message;
 }
 
+TEST_F(SubgraphSnippetSerializationTest, SerializeSubgraphBody) {
+    SKIP_IF_CURRENT_TEST_IS_DISABLED()
+
+    auto model_add = ([] () -> std::shared_ptr<ov::Model> {
+        auto shape = ov::Shape({2, 2});
+        auto input0 = std::make_shared<ov::op::v0::Parameter>(ov::element::f32, shape);
+        auto input1 = std::make_shared<ov::op::v0::Parameter>(ov::element::f32, shape);
+        auto ininput0 = std::make_shared<ov::op::v0::Parameter>(ov::element::f32, shape);
+        auto ininput1 = std::make_shared<ov::op::v0::Parameter>(ov::element::f32, shape);
+        auto add = std::make_shared<ov::op::v1::Add>(ininput0, ininput1);
+        auto subgraph_body = std::make_shared<ov::Model>(ov::NodeVector{add}, ov::ParameterVector{ininput0, ininput1});
+        auto subgraph = std::make_shared<ngraph::snippets::op::Subgraph>(ov::NodeVector{input0, input1}, ov::clone_model(*subgraph_body.get()));
+        return std::make_shared<ov::Model>(ov::NodeVector{subgraph}, ov::ParameterVector{input0, input1});
+    })();
+
+    auto model_divide = ([] () -> std::shared_ptr<ov::Model> {
+        auto shape = ov::Shape({2, 2});
+        auto input0 = std::make_shared<ov::op::v0::Parameter>(ov::element::f32, shape);
+        auto input1 = std::make_shared<ov::op::v0::Parameter>(ov::element::f32, shape);
+        auto ininput0 = std::make_shared<ov::op::v0::Parameter>(ov::element::f32, shape);
+        auto ininput1 = std::make_shared<ov::op::v0::Parameter>(ov::element::f32, shape);
+        auto add = std::make_shared<ov::op::v1::Divide>(ininput0, ininput1);
+        auto subgraph_body = std::make_shared<ov::Model>(ov::NodeVector{add}, ov::ParameterVector{ininput0, ininput1});
+        auto subgraph = std::make_shared<ngraph::snippets::op::Subgraph>(ov::NodeVector{input0, input1}, ov::clone_model(*subgraph_body.get()));
+        return std::make_shared<ov::Model>(ov::NodeVector{subgraph}, ov::ParameterVector{input0, input1});
+    })();
+    ov::Core core;
+    ov::CompiledModel compiled_model_add = core.compile_model(model_add, "CPU");
+    std::stringstream stream;
+    compiled_model_add.export_model(stream);
+    ov::CompiledModel imported_compiled_model_add = core.import_model(stream, "CPU");
+    auto compiled_model_runtime_add = ov::clone_model(*compiled_model_add.get_runtime_model());
+    auto imported_compiled_model_runtime_add = ov::clone_model(*imported_compiled_model_add.get_runtime_model());
+
+    {
+        std::ofstream out_stream;
+        // this model can be read with benchmark_app afterwards
+        out_stream.open("compiled_model_add.blob");
+        compiled_model_add.export_model(out_stream);
+        out_stream.close();
+        ov::pass::Serialize("compiled_model_add.xml", "compiled_model_add.bin").run_on_model(compiled_model_runtime_add);
+    }
+    {
+        std::ofstream out_stream;
+        out_stream.open("imported_model_add.blob");
+        imported_compiled_model_add.export_model(out_stream);
+        out_stream.close();
+        ov::pass::Serialize("imported_model_add.xml", "imported_model_add.bin").run_on_model(imported_compiled_model_runtime_add);
+    }
+    const auto fc = FunctionsComparator::with_default()
+            .enable(FunctionsComparator::CONST_VALUES)
+            .enable(FunctionsComparator::ATTRIBUTES);
+    const auto results = fc.compare(compiled_model_runtime_add, imported_compiled_model_runtime_add);
+
+    ASSERT_TRUE(results.valid) << results.message;
+
+    ov::CompiledModel compiled_model_divide = core.compile_model(model_divide, "CPU");
+    auto compiled_model_runtime_divide = ov::clone_model(*compiled_model_divide.get_runtime_model());
+    const auto results_false = fc.compare(compiled_model_runtime_divide, imported_compiled_model_runtime_add);
+    ASSERT_FALSE(results_false.valid) << results.message;
+    }
+
 TEST_F(SubgraphSnippetSerializationTest, SerializeSubgraphWithScalarConst) {
     SKIP_IF_CURRENT_TEST_IS_DISABLED()
     auto model = ([] () -> std::shared_ptr<ov::Model> {
