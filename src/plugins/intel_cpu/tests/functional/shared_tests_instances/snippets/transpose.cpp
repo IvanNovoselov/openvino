73ec12a1b9bf1fd63477f7fc40b675b6eb954c9d
Ivan Novoselov <ivan.novoselov@intel.com>
2022-10-25 13:43:36 +0200

    Transpose Tokenizatio test (failing, since not tokenized)

---
+++ src/plugins/intel_cpu/tests/functional/shared_tests_instances/snippets/transpose.cpp
@@ -0,0 +1,27 @@
+// Copyright (C) 2022 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "snippets/transpose.hpp"
+#include "common_test_utils/test_constants.hpp"
+
+namespace ov {
+namespace test {
+namespace snippets {
+
+
+namespace {
+
+INSTANTIATE_TEST_SUITE_P(smoke_Snippets_Transpose, Transpose,
+                     ::testing::Combine(
+                             ::testing::Values(ov::PartialShape {1, 42, 16, 64}),
+                             ::testing::Values(std::vector<int> {0, 2,  3, 1}),
+                             ::testing::Values(2), // Sinh + Transpose
+                             ::testing::Values(1), // Tokenized Transpose
+                             ::testing::Values(CommonTestUtils::DEVICE_CPU)),
+                     Transpose::getTestCaseName);
+
+}  // namespace
+} // namespace snippets
+} // namespace test
+} // namespace ov
---
+++ src/tests/functional/plugin/shared/src/snippets/transpose.cpp
@@ -0,0 +1,48 @@
+// Copyright (C) 2022 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "common_test_utils/common_utils.hpp"
+#include "snippets/transpose.hpp"
+#include "subgraph_permute.hpp"
+#include "functional_test_utils/skip_tests_config.hpp"
+
+namespace ov {
+namespace test {
+namespace snippets {
+
+std::string Transpose::getTestCaseName(testing::TestParamInfo<ov::test::snippets::TransposeParams> obj) {
+    ov::PartialShape inputShape;
+    std::vector<int> order;
+    std::string targetDevice;
+    size_t num_nodes, num_subgraphs;
+    std::tie(inputShape, order, num_nodes, num_subgraphs, targetDevice) = obj.param;
+
+    std::ostringstream result;
+    result << "IS=" << CommonTestUtils::partialShape2str({inputShape}) << "_";
+    result << "Order=" << CommonTestUtils::vec2str(order) << "_";
+    result << "#N=" << num_nodes << "_";
+    result << "#S=" << num_subgraphs << "_";
+    result << "targetDevice=" << targetDevice;
+    return result.str();
+}
+
+void Transpose::SetUp() {
+    ov::PartialShape inputShape;
+    std::vector<int> order;
+    std::tie(inputShape, order, ref_num_nodes, ref_num_subgraphs, targetDevice) = this->GetParam();
+    init_input_shapes({{{inputShape}, {inputShape.get_shape(), }}});
+
+    auto f = ov::test::snippets::TransposeSinhFunction({inputShape}, order);
+    function = f.getOriginal();
+}
+
+TEST_P(Transpose, CompareWithRefImpl) {
+    run();
+    ov::pass::Serialize("transpose_test.xml", "transpose_tests.bin").run_on_model(function);
+    validateNumSubgraphs();
+}
+
+} // namespace snippets
+} // namespace test
+} // namespace ov
