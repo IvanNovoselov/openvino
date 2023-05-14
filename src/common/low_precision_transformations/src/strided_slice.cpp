901250a0348e8916a230cb59bb03864268c76fdd
eshoguli <edward.shogulin@intel.com>
2023-05-15 00:23:12 +0100

    [LPT] StridedSlice dequantization improvement

--- src/common/low_precision_transformations/src/strided_slice.cpp
+++ src/common/low_precision_transformations/src/strided_slice.cpp
@@ -11,6 +11,8 @@
 #include "low_precision/network_helper.hpp"
 #include "itt.hpp"
 
+#include "ngraph/pass/serialize.hpp"
+
 namespace ngraph {
 namespace pass {
 namespace low_precision {
@@ -20,62 +22,77 @@ namespace {
 std::shared_ptr<opset1::Constant> stridedSliceDeqConstant(
     const std::shared_ptr<ngraph::Node> strSlice,
     const std::shared_ptr<ngraph::Node> dequantizaitonConstant) {
-    auto constant = ov::as_type_ptr<ngraph::opset1::Constant>(dequantizaitonConstant);
-    auto constantShape = constant->get_shape();
-    if (shape_size(constantShape) == 1ul) {
+    // step #1: prerequisistes
+    const auto constant = ov::as_type_ptr<ngraph::opset1::Constant>(dequantizaitonConstant);
+    const auto& original_constant_shape = constant->get_shape();
+    if (shape_size(original_constant_shape) == 1ul) {
         return NetworkHelper::toScalar(constant);
     }
 
-    const auto stridedSlicePShape = strSlice->get_input_partial_shape(0);
-    const size_t rank = stridedSlicePShape.rank().get_length();
-    if (rank != constantShape.size()) {
-        ngraph::Shape newConstantShape;
-        if (ngraph::shape_size(constantShape) == 1) {
+    const auto strided_slice = ov::as_type_ptr<ngraph::opset1::StridedSlice>(strSlice);
+
+    // step #2: aligh shapes
+    std::shared_ptr<ngraph::opset1::Constant> new_constant = constant;
+    const size_t rank = strSlice->get_input_partial_shape(0).rank().get_length();
+    ngraph::Shape newConstantShape = original_constant_shape;
+    if (rank != newConstantShape.size()) {
+        if (ngraph::shape_size(original_constant_shape) == 1) {
             newConstantShape = ngraph::Shape(rank, 1);
         } else {
-            newConstantShape = constantShape;
+            newConstantShape = original_constant_shape;
 
             // case when constShape without batch
-            if ((constantShape.size() > 1) &&
-                (constantShape.size() < rank)) {
+            if ((original_constant_shape.size() > 1) &&
+                (original_constant_shape.size() < rank)) {
                 newConstantShape.insert(newConstantShape.begin(), 1);
             }
         }
-        constantShape = newConstantShape;
 
-        const auto newConstant = fold<ngraph::opset1::Broadcast>(
-            constant,
-            ngraph::opset1::Constant::create(ngraph::element::i32, { newConstantShape.size() }, newConstantShape));
-        constant = ov::as_type_ptr<ngraph::opset1::Constant>(newConstant);
+        if (original_constant_shape != newConstantShape) {
+            const auto newConstant = fold<ngraph::opset1::Broadcast>(
+                constant,
+                ngraph::opset1::Constant::create(ngraph::element::i32, { newConstantShape.size() }, newConstantShape));
+            new_constant = ov::as_type_ptr<ngraph::opset1::Constant>(newConstant);
+        }
     }
 
-    const auto stridedSlice = ov::as_type_ptr<ngraph::opset1::StridedSlice>(strSlice);
+    // step #3: update original begin & end & strides
+    auto begin = ov::as_type_ptr<opset1::Constant>(strided_slice->get_input_node_shared_ptr(1))->cast_vector<int64_t>();
+    auto end = ov::as_type_ptr<opset1::Constant>(strided_slice->get_input_node_shared_ptr(2))->cast_vector<int64_t>();
+    auto strides = ov::as_type_ptr<opset1::Constant>(strided_slice->get_input_node_shared_ptr(3))->cast_vector<int64_t>();
+    for (auto i = 0ull; i < newConstantShape.size(); ++i) {
+        if (newConstantShape[i] == 1ull) {
+            if (i < begin.size()) {
+                begin[i] = 0;
+            }
+            if (i < end.size()) {
+                end[i] = 1;
+            }
 
-    auto beginMask = stridedSlice->get_begin_mask();
-    auto endMask = stridedSlice->get_end_mask();
-    for (size_t i = 0; i < constantShape.size(); ++i) {
-        if ((beginMask.size() <= i) && (endMask.size() <= i)) {
-            break;
-        }
-        // don't slice constant if current dimension is 1
-        if (constantShape[i] == 1ul) {
-            beginMask[i] = 1ul;
-            endMask[i] = 1ul;
+            if (i < strides.size()) {
+                strides[i] = 1;
+            }
         }
     }
 
     const auto result = fold<ngraph::opset1::StridedSlice>(
-        constant,
-        stridedSlice->input_value(1),
-        stridedSlice->input_value(2),
-        stridedSlice->input_value(3),
-        beginMask,
-        endMask,
-        stridedSlice->get_new_axis_mask(),
-        stridedSlice->get_shrink_axis_mask(),
-        stridedSlice->get_ellipsis_mask());
+        new_constant,
+        std::make_shared<opset1::Constant>(element::i64, Shape{ begin.size() }, begin),
+        std::make_shared<opset1::Constant>(element::i64, Shape{ end.size() }, end),
+        std::make_shared<opset1::Constant>(element::i64, Shape{ strides.size() }, strides),
+        strided_slice->get_begin_mask(),
+        strided_slice->get_end_mask(),
+        strided_slice->get_new_axis_mask(),
+        strided_slice->get_shrink_axis_mask(),
+        strided_slice->get_ellipsis_mask());
 
-    return ov::as_type_ptr<opset1::Constant>(NetworkHelper::toScalarIfPossible(result));
+    new_constant = ov::as_type_ptr<opset1::Constant>(NetworkHelper::toScalarIfPossible(result));
+
+    if (shape_size(new_constant->get_shape()) == 1ul) {
+        return NetworkHelper::toScalar(new_constant);
+    }
+
+    return new_constant;
 }
 
 } // namespace
@@ -105,6 +122,16 @@ bool StridedSliceTransformation::transform(TransformationContext& context, ngrap
     auto dequantization = NetworkHelper::getDequantization(stridedSlice, defaultPrecisions);
 
     if (dequantization.subtract) {
+        ngraph::pass::VisualizeTree("svg/lpt.strided_slice.1.svg").run_on_model(ov::Model::global_model);
+        ngraph::pass::Serialize("svg/lpt.strided_slice.1.xml", "svg/cpu.strided_slice.1.bin").run_on_model(ov::Model::global_model);
+
+        if (stridedSlice->get_friendly_name() == "Gs/_Run/Gs/G_synthesis/4x4/Conv/strided_slice") {
+            std::cout << "StridedSliceTransformation::transform: " << stridedSlice->get_friendly_name() << std::endl;
+        }
+        if (stridedSlice->get_friendly_name() == "Gs/_Run/Gs/G_synthesis/4x4/ToRGB/strided_slice") {
+            std::cout << "StridedSliceTransformation::transform: " << stridedSlice->get_friendly_name() << std::endl;
+        }
+
         const auto newSubConst = stridedSliceDeqConstant(stridedSlice, dequantization.subtractConstant);
         replace_node(dequantization.subtractConstant, newSubConst);
         dequantization.subtractConstant = newSubConst;
@@ -128,10 +155,30 @@ bool StridedSliceTransformation::canBeTransformed(const TransformationContext& c
         return false;
     }
 
-    if (operation->get_input_partial_shape(0).rank().is_dynamic() &&
+    const auto input_shape = operation->get_input_partial_shape(0);
+    if (input_shape.rank().is_dynamic() &&
         ((dequantization.subtract && ngraph::shape_size(dequantization.subtractConstant->get_shape()) > 1) ||
          (dequantization.multiply && ngraph::shape_size(dequantization.multiplyConstant->get_shape()) > 1))) {
         return false;
+    }
+
+    if (input_shape.is_dynamic()) {
+        //for (auto i = 0ull; i < input_shape.rank().get_length(); ++i) {
+        //    // TODO: check operation parameters
+        //    if (input_shape[i].is_dynamic() &&
+        //        ((dequantization.subtract && (ngraph::shape_size(dequantization.subtractConstant->get_shape()) > 1) && (dequantization.subtractConstant->get_shape()[i] > 1)) ||
+        //        (dequantization.multiply && (ngraph::shape_size(dequantization.multiplyConstant->get_shape()) > 1) && (dequantization.multiplyConstant->get_shape()[i] > 1)))) {
+        //        return false;
+        //    }
+        //}
+
+        //const auto stridedSlice = ov::as_type_ptr<ngraph::opset1::StridedSlice>(operation);
+        //const auto& shrink_axis_mask = stridedSlice->get_shrink_axis_mask();
+        //if (std::any_of(shrink_axis_mask.begin(), shrink_axis_mask.end(), [](const int64_t value) { return value != 0; })) {
+        //    // TODO: dynamic shape
+        //    const ngraph::Shape data_shape = stridedSlicePShapeIn.to_shape();
+        //    newConstantShape = data_shape;
+        //}
     }
 
     return true;
