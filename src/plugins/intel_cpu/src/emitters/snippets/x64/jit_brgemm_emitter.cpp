8347c67a03eacc40df8234534c7a0393d6a6c2eb
Ivan Novoselov <ivan.novoselov@intel.com>
2024-06-24 18:40:04 +0100

    Dirst implementation through references

--- src/plugins/intel_cpu/src/emitters/snippets/x64/jit_brgemm_emitter.cpp
+++ src/plugins/intel_cpu/src/emitters/snippets/x64/jit_brgemm_emitter.cpp
@@ -28,10 +28,9 @@ jit_brgemm_emitter::jit_brgemm_emitter(jit_generator* h, cpu_isa_t isa,
 
     const auto& brg0Prc = brgemm_node->get_input_element_type(0);
     const auto& brg1Prc = brgemm_node->get_input_element_type(1);
-    auto kernel_config = std::make_shared<BrgemmKernelConfig>(brg0Prc, brg1Prc,
-                                                            brgemm_node->get_beta(),
-                                                            brgemm_node->is_amx(),
-                                                            brgemm_node->is_with_compensations());
+    BrgemmKernelConfig kernel_config(brg0Prc, brg1Prc,
+                                      brgemm_node->get_beta(), brgemm_node->is_amx(),
+                                      brgemm_node->is_with_compensations());
     m_kernel_executor = kernel_table->register_kernel<BrgemmKernelExecutor>(expr,
                                                                             compiled_kernel_cache,
                                                                             kernel_config);
