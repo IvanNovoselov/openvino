cc297c77b96df9c0a842318785e8adc4dc2dc860
Ivan Novoselov <ivan.novoselov@intel.com>
2024-10-24 14:05:52 +0100

    Generalize update_loop_info for loops with several unique repacking ops

--- src/plugins/intel_cpu/src/transformations/snippets/x64/pass/lowered/adjust_brgemm_copy_b_loop_ports.cpp
+++ src/plugins/intel_cpu/src/transformations/snippets/x64/pass/lowered/adjust_brgemm_copy_b_loop_ports.cpp
@@ -16,10 +16,9 @@ namespace intel_cpu {
 
 bool pass::AdjustBrgemmCopyBLoopPorts::update_loop_info(const std::shared_ptr<snippets::lowered::UnifiedLoopInfo>& loop_info) {
     OPENVINO_ASSERT(loop_info, "Invalid loop info pointer");
-    snippets::lowered::UnifiedLoopInfo::LoopPortDesc *copy_b_loop_desc = nullptr;
+    std::vector<std::pair<snippets::lowered::UnifiedLoopInfo::LoopPortDesc*, ov::element::Type>> copy_b_loop_desc;
     bool first_port = true;
     bool first_port_incremented = false;
-    ov::element::Type precision;
     auto caller = [&](snippets::lowered::LoopPort &loop_port,
                       snippets::lowered::UnifiedLoopInfo::LoopPortDesc &loop_desc) {
         const auto& p = *loop_port.expr_port;
@@ -27,9 +26,10 @@ bool pass::AdjustBrgemmCopyBLoopPorts::update_loop_info(const std::shared_ptr<sn
             p.get_index() == 1) {
             const auto& node = p.get_expr()->get_node();
             if (auto brg = as_type_ptr<BrgemmCPU>(node)) {
-                if (brgemm_utils::with_repacking(brg->get_type())) {
-                    copy_b_loop_desc = &loop_desc;
-                    precision = node->get_input_element_type(1);
+                const auto precision = node->get_input_element_type(1);
+                if (brgemm_utils::with_repacking(brg->get_type()) &&
+                    precision != element::f32) {
+                    copy_b_loop_desc.push_back({&loop_desc, precision});
                 }
             }
         }
@@ -40,29 +40,29 @@ bool pass::AdjustBrgemmCopyBLoopPorts::update_loop_info(const std::shared_ptr<sn
     };
     loop_info->iterate_through_infos(caller);
 
-    if (copy_b_loop_desc && precision != element::f32) {
+    for (const auto& desc :  copy_b_loop_desc) {
+        const auto loop_desc = desc.first;
+        const auto precision = desc.second;
         // K blocking loop
         if (first_port_incremented) {
-            const auto ptr_incr = copy_b_loop_desc->ptr_increment;
+            const auto ptr_incr = loop_desc->ptr_increment;
             const auto blocked_shape_ptr_inc = brgemm_utils::repacking::compute_out_leading_dim(ptr_incr, precision);
             if (ptr_incr != 0 && ptr_incr != blocked_shape_ptr_inc) {
-                copy_b_loop_desc->ptr_increment = blocked_shape_ptr_inc;
-                OPENVINO_ASSERT(copy_b_loop_desc->finalization_offset % ptr_incr == 0,
+                loop_desc->ptr_increment = blocked_shape_ptr_inc;
+                OPENVINO_ASSERT(loop_desc->finalization_offset % ptr_incr == 0,
                                 "Can't rescale finalization offsets");
-                copy_b_loop_desc->finalization_offset = copy_b_loop_desc->ptr_increment *
-                                                        (copy_b_loop_desc->finalization_offset / ptr_incr);
+                loop_desc->finalization_offset = loop_desc->ptr_increment *
+                                                 (loop_desc->finalization_offset / ptr_incr);
             }
-        // N blocking loop
+            // N blocking loop
         } else {
-            int64_t k_blk_size = 4 / static_cast<int64_t>(precision.size());
-            copy_b_loop_desc->ptr_increment =
-                    snippets::utils::dynamic_safe_mul(copy_b_loop_desc->ptr_increment, k_blk_size);
-            copy_b_loop_desc->finalization_offset =
-                    snippets::utils::dynamic_safe_mul(copy_b_loop_desc->finalization_offset, k_blk_size);
+            auto k_blk_size = static_cast<int64_t>(brgemm_utils::compute_vnni_factor(precision));
+            loop_desc->ptr_increment = snippets::utils::dynamic_safe_mul(loop_desc->ptr_increment, k_blk_size);
+            loop_desc->finalization_offset = snippets::utils::dynamic_safe_mul(loop_desc->finalization_offset, k_blk_size);
         }
-        return true;
     }
-    return false;
+
+    return !copy_b_loop_desc.empty();
 }
 
 bool pass::AdjustBrgemmCopyBLoopPorts::run(const snippets::lowered::LinearIR& linear_ir) {
